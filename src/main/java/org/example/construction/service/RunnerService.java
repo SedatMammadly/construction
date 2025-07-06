6c872ed07d46f832d7133c069eb63a707c6f9bc1
SarKhann <serxan.babayev.06@gmail.com>
2025-07-06 10:55:20 +0400

    API Optimized

--- src/main/java/org/example/construction/service/RunnerService.java
+++ src/main/java/org/example/construction/service/RunnerService.java
@@ -3,21 +3,29 @@ package org.example.construction.service;
 import lombok.RequiredArgsConstructor;
 import org.example.construction.authentication.AuthenticationService;
 import org.example.construction.dto.AuthRequest;
+import org.example.construction.model.User;
+import org.example.construction.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RunnerService implements CommandLineRunner {
     private final AuthenticationService authenticationService;
-
+private final UserRepository userRepository;
     @Override
     public void run(String... args) throws Exception {
-        AuthRequest authRequest = new AuthRequest();
-        authRequest.setUsername("serxanbabayev614@gmail.com");
-        authRequest.setPassword("Serxan3232");
-        System.out.println(authRequest.getUsername() + " " + authRequest.getPassword());
-        authenticationService.register(authRequest);
+        List<User> userList=userRepository.findAll();
+        if (userList.isEmpty()){
+            AuthRequest authRequest = new AuthRequest();
+            authRequest.setUsername("serxanbabayev614@gmail.com");
+            authRequest.setPassword("Serxan3232");
+            System.out.println(authRequest.getUsername() + " " + authRequest.getPassword());
+            authenticationService.register(authRequest);
+        }
+
     }
 
 
